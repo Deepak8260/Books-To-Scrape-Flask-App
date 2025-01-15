d951de9ed828f926b2ae448d86b73282e0c83729
Deepak Kumar Mohanty <kd.codegeek@gmail.com>
2025-01-15 13:07:58 +0530

    version 1

--- app.py
+++ app.py
@@ -1,94 +1,40 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template
 import requests
 from bs4 import BeautifulSoup as bs
 
 app = Flask(__name__)
 
-# List of categories
-categories = {
-    "Travel": "travel",
-    "Mystery": "mystery",
-    "Historical Fiction": "historical-fiction",
-    "Sequential Art": "sequential-art",
-    "Classics": "classics",
-    "Philosophy": "philosophy",
-    "Romance": "romance",
-    "Womens Fiction": "womens-fiction",
-    "Fiction": "fiction",
-    "Childrens": "childrens",
-    "Religion": "religion",
-    "Nonfiction": "nonfiction",
-    "Music": "music",
-    "Default": "default",
-    "Science Fiction": "science-fiction",
-    "Sports and Games": "sports-and-games",
-    "Fantasy": "fantasy",
-    "New Adult": "new-adult",
-    "Young Adult": "young-adult",
-    "Science": "science",
-    "Poetry": "poetry",
-    "Paranormal": "paranormal",
-    "Art": "art",
-    "Psychology": "psychology",
-    "Autobiography": "autobiography",
-    "Parenting": "parenting",
-    "Adult Fiction": "adult-fiction",
-    "Humor": "humor",
-    "Horror": "horror",
-    "History": "history",
-    "Food and Drink": "food-and-drink",
-    "Christian Fiction": "christian-fiction",
-    "Business": "business",
-    "Biography": "biography",
-    "Thriller": "thriller",
-    "Contemporary": "contemporary",
-    "Spirituality": "spirituality",
-    "Academic": "academic",
-    "Self Help": "self-help",
-    "Historical": "historical",
-    "Christian": "christian",
-    "Suspense": "suspense",
-    "Short Stories": "short-stories",
-    "Novels": "novels",
-    "Health": "health",
-    "Politics": "politics",
-    "Cultural": "cultural",
-    "Erotica": "erotica",
-    "Crime": "crime"
-}
-
-@app.route('/', methods=['GET', 'POST'])
+@app.route('/')
 def home():
-    selected_category = request.form.get('category')
-    all_books = []
+    # Scrape the data
+    x = requests.get('https://books.toscrape.com/catalogue/category/books/music_14/index.html')
+    soup = bs(x.text, 'html.parser')
+    books = soup.find_all('li', {'class': 'col-xs-6 col-sm-4 col-md-3 col-lg-3'})
 
-    if selected_category:
-        category_url = categories[selected_category]
-        url = f'https://books.toscrape.com/catalogue/category/books/{category_url}_1/index.html'
-        x = requests.get(url)
-        soup = bs(x.text, 'html.parser')
-        books = soup.find_all('li', {'class': 'col-xs-6 col-sm-4 col-md-3 col-lg-3'})
+    # Extract prices and convert to INR
+    prices = []
+    conversion_rate = 105.29  # Example conversion rate from GBP to INR
+    for i in books:
+        price = i.find_all('p', {'class': 'price_color'})[0].text
+        price = price.replace('Â£', '')
+        prices.append(round(float(price) * conversion_rate, 2))
 
-        for i in books:
-            # Extract price and convert to INR
-            price = i.find_all('p', {'class': 'price_color'})[0].text
-            price = price.replace('Â£', '')
-            price_inr = round(float(price) * 105.29, 2)
+    # Extract titles
+    titles = [i.h3.a['title'] for i in books]
 
-            # Extract title
-            title = i.h3.a['title']
+    # Extract ratings
+    num_map = {'One': 1, 'Two': 2, 'Three': 3, 'Four': 4, 'Five': 5}
+    ratings = [num_map.get(i.article.p['class'][1]) for i in books]
 
-            # Extract rating
-            num_map = {'One': 1, 'Two': 2, 'Three': 3, 'Four': 4, 'Five': 5}
-            rating = num_map.get(i.article.p['class'][1])
+    # Extract images
+    imgs = []
+    for i in books:
+        l = i.img['src'].replace('../', '')
+        img = 'https://books.toscrape.com/' + l
+        imgs.append(img)
 
-            # Extract image
-            l = i.img['src'].replace('../', '')
-            img = 'https://books.toscrape.com/' + l
-
-            all_books.append((title, price_inr, rating, img))
-
-    return render_template('books.html', books=all_books, categories=categories.keys())
+    # Render the template with data and pass zip function
+    return render_template('books.html', titles=titles, prices=prices, ratings=ratings, imgs=imgs, zip=zip)
 
 if __name__ == '__main__':
     app.run(debug=True)
